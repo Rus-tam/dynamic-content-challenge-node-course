741003c75010c3279f9efa77edee092904896ae7
Rustam <ahmetov.rustam2011@gmail.com>
2021-08-10 16:26:25 +0500

    'initial_commit'

---
+++ app.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const express = require('express');
+const hbs = require('hbs');
+const indexRouter = require('./routes/index');
+const userRouter = require('./routes/users');
+
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+//Define path for Express config
+const publicDirectoryPath = path.join(__dirname, '/public');
+const viewPath = path.join(__dirname, '/views');
+const layoutPath = path.join(__dirname, '/views/layouts');
+
+//Setup handlebars engine and views location
+app.set('view engine', 'hbs');
+app.set('views', viewPath);
+hbs.registerPartials(layoutPath);
+
+app.use(indexRouter);
+app.use(userRouter);
+
+
+
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}`);
+});
